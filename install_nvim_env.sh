6264cb701eed0e5987345c6c4671e2f9239c3d82
wanghuaidong <wang.huaidong@foxmail.com>
2020-11-28 14:35:28 +0800

    change nvim ppa to unstable to get lastest version

--- install_nvim_env.sh
+++ install_nvim_env.sh
@@ -1,6 +1,7 @@
 
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
 
